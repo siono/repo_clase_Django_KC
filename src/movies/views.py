f6060115d84c2fe1b004d26beb5af555b73d8731
José Carlos <jc.esteban.gomez@gmail.com>
2017-12-14 21:19:59 +0100

    Pintamos las 5 películas mas actuales en la home

--- src/movies/views.py
+++ src/movies/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from movies.models import Movie
 
 
 def hello_world(request):
@@ -7,3 +10,10 @@ def hello_world(request):
         return HttpResponse("Hello world")
     else:
         return HttpResponse("Hello " + name)
+
+def home(request):
+    latest_movies = Movie.objects.all().order_by("-release_date")[:5] #ordena por orden descendente por el campo release date.
+                                                                  #  Fijarse que pone un menos. Si queremos por ordes ascendiente se pone sin el menos
+
+    context = {'movies': latest_movies}
+    return render(request, "home.html",context)
