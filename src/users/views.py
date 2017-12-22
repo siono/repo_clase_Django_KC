a435df0c871165bfb7899ec98b46e2c6aa4dd314
José Carlos <jc.esteban.gomez@gmail.com>
2017-12-22 19:39:57 +0100

    Cambiamos nuestro formulario clasico por un Django Form

--- src/users/views.py
+++ src/users/views.py
@@ -3,23 +3,28 @@ from django.contrib.auth import authenticate,login as django_login, logout as dj
 from django.shortcuts import render, redirect
 from django.views import View
 
+from users.forms import LoginForm
+
 
 class LoginView(View):
 
     def get(self,request):
-        return render(request, "login_form.html")
+        context = {'form': LoginForm()}
+        return render(request, "login_form.html", context)
 
     def post(self,request):
-        username = request.POST.get("login_username")
-        password = request.POST.get("login_password")
-        # possible_usesr = User.objects.filter(username=username, password=password)  --> no lo puedo hacer asi porque la password hay que pasarla hasheada
-        authenticate_user = authenticate(username=username, password=password)
-        if authenticate_user and authenticate_user.is_active:
-            django_login(request, authenticate_user)
-            return redirect('home_page')
-        else:
-            messages.error(request, "Usuario incorrecto o inactivo")
-            return render(request, "login_form.html")
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("login_username")
+            password = form.cleaned_data.get("login_password")
+            # possible_request.POST.get(usesr = User.objects.filter(username=username, password=password)  --> no lo puedo hacer asi porque la password hay que pasarla hasheada
+            authenticate_user = authenticate(username=username, password=password)
+            if authenticate_user and authenticate_user.is_active:
+                django_login(request, authenticate_user)
+                return redirect('home_page')
+            else:
+                messages.error(request, "Usuario incorrecto o inactivo")
+        return render(request, "login_form.html", {'form': form})
 
 
 
