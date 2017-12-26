174314c6f021abcad084bccc91b553e9c5d72046
José Carlos <jc.esteban.gomez@gmail.com>
2017-12-26 17:44:22 +0100

    añadimos funcion post al endpoint helloworld

--- src/users/api.py
+++ src/users/api.py
@@ -6,3 +6,7 @@ class HelloWorld(APIView):
 
     def get(self,request):
         return Response(['hello', 'world'])
+
+    def post(self,request):
+        return Response(request.data) # en rest_framework los datos que me envian `
+                                      # siempre se accede desde la varialbe `data
