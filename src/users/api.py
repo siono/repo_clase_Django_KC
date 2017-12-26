575bf87eae31f0a9a8962805c03b50740c677a8e
José Carlos <jc.esteban.gomez@gmail.com>
2017-12-26 19:07:58 +0100

    Creamos endpoint de creación de usuarios

--- src/users/api.py
+++ src/users/api.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -21,3 +22,11 @@ class UsersListAPI(APIView):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
+
+    def post(self,request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()#llamo a la funcion del serializer del Usuario
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- src/users/serializers.py
+++ src/users/serializers.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 
 class UserSerializer(serializers.Serializer):
@@ -10,8 +12,21 @@ class UserSerializer(serializers.Serializer):
     email = serializers.CharField()
     password = serializers.CharField()
 
+    def validate_username(self, data): #para hacer una validación mas especifica ponemos una funcion validate_<campo>
+        if User.objects.filter(username=data).exists():
+            raise ValidationError("User already exists") #en Django se lanza las excepciones con la palabra reservada 'raise'
+        return data
+
     def create(self, validated_data):
-        pass
+        instance = User()
+        instance.first_name = validated_data.get('first_name')
+        instance.last_name = validated_data.get('last_name')
+        instance.username = validated_data.get('username')
+        instance.email = validated_data.get('email')
+        instance.password = validated_data.get('password')
+        instance.set_password(validated_data.get('password'))#tenemos que hashear la contraseña. Utilizamos el metodo que nos da Django
+        instance.save()
+        return instance
 
     def update(self, instance, validated_data):
         pass
