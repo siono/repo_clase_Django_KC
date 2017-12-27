4194b6617d5cbccd868eecca5e2fb570b905b7bb
José Carlos <jc.esteban.gomez@gmail.com>
2017-12-27 13:20:45 +0100

    Creamos los endpoint de detalle, actulización y borrado de una pelicula

--- src/movies/api.py
+++ src/movies/api.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
 
 
@@ -8,5 +8,10 @@ from movies.serializers import MovieSerializer
 #creamos el endpoint de creación y listado de peliculas utilizando listas genericas
 class MoviesListAPI(ListCreateAPIView):
 
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+#endpoint actualización y eliminación
+class MovieDetailAPI(RetrieveUpdateDestroyAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
