a5d1598a7e6ea44129539fbb076ae46a51313a72
José Carlos <jc.esteban.gomez@gmail.com>
2017-12-27 14:32:54 +0100

    Utilizamos diferentes serializadores para el listado y creación de peliculas

--- src/movies/api.py
+++ src/movies/api.py
@@ -3,13 +3,17 @@ from rest_framework.response import Response
 
 
 from movies.models import Movie
-from movies.serializers import MovieSerializer
+from movies.serializers import MovieSerializer, MoviesListSerializer
+
 
 #creamos el endpoint de creación y listado de peliculas utilizando listas genericas
 class MoviesListAPI(ListCreateAPIView):
 
     queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
+
+    #como al crear una pelicula necesito que coja todos los campos nos redifinos la funcion
+    def get_serializer_class(self):
+        return MoviesListSerializer if self.request.method == "GET" else MovieSerializer #operador ternario
 
 #endpoint actualización y eliminación
 class MovieDetailAPI(RetrieveUpdateDestroyAPIView):
--- src/movies/serializers.py
+++ src/movies/serializers.py
@@ -3,8 +3,15 @@ from rest_framework import serializers
 from movies.models import Movie
 
 
+class MoviesListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Movie
+        fields = ["id","image","title"]
+
+
 class MovieSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Movie
-        fields = '__all__'
+        fields = "__all__"
